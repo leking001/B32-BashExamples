4d54aa06ea19ca32bbcc28b3ab5dd0a1f283066a
Miguel Dres-Barrientos <miguel4696@hotmail.com>
2018-12-03 10:02:37 -0500

    fini avec les variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+
+#Pas d'espce dans lassignation de variable
+prenom=KimCharles
+echo $prenom
+
+#Forcer une déclaration d'une variable "int"
+declare -i age=33
+
+#Mettre une variable READ ONLY 
+declare -r age2=33
+
+#Les variable système
+echo $0
+
+echo $1 #Paramètre 1
+echo $2 #Paramètre 2
+
+echo $#
+
