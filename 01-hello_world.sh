6513e7923f2d25c10949d9729ea923a28cea8f30
Miguel Dres-Barrientos <miguel4696@hotmail.com>
2018-12-03 09:49:50 -0500

    Fini avec 01-hello world

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+#Ceci est un commentaire
+echo "Salut"
+
+#-n = empêche retour de ligne 
+echo -n "vive "
+echo  " Linux"
+
+#avtive les caratères spéciaux
+echo -e
+
+#read -p = cin et pour savoir la valeur dans une variable on met $ avant la varible 
+read -p "Vous avez quel âge?" age
+echo $age "ans! Vous paraissez plus jeune"
