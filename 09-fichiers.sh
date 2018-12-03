45716c7298480ae855d1471b4e5d415ba5155cff
Miguel Dres-Barrientos <miguel4696@hotmail.com>
2018-12-03 11:19:39 -0500

    Fini avec 09-fichier.sh

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+#$1 premier argument afficher 
+#-e pour savoir s'il existe
+#-d pour savoir si c'est un répertoire
+
+if test -e $1
+then 
+	echo "existe"
+
+	if test -d $1
+	then
+		echo "Il est un répertoire"
+	fi
+else 
+	echo "inexistant"
+fi
