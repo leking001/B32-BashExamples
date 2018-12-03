63b52f494e75e067bfe9ba2609a491690e241824
Miguel Dres-Barrientos <miguel4696@hotmail.com>
2018-12-03 11:11:59 -0500

    Fini avec 08-regexp

--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+echo -n "Texte : "
+read texte
+
+#~ expréssion régulière et ^commence par... et [a-z] entre et + au moins 1 caractère après infini et $ fini
+if [[ $texte =~ ^[0-9]+$ ]]
+then
+	echo "Numérique"
+fi
