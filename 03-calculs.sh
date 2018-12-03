e421df2040b180fa0a80f2e35ff9e596f16f6399
Miguel Dres-Barrientos <miguel4696@hotmail.com>
2018-12-03 10:09:53 -0500

    fini avec les calculs

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+#cout de 10 +19
+echo 10+19
+
+#on fait le calcul 10 +19 avec les parenthèses et le signe $
+echo $((10+19))
+
+
+read -p "Nombre 1 :" premier
+read -p "Nombre 2 :" second 
+
+let resultat=$premier*$second
+
+echo $resultat
