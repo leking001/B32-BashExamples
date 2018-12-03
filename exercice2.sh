660c8500126dc83a7d0b47ea79418dbe72e42d2d
Miguel Dres-Barrientos <miguel4696@hotmail.com>
2018-12-03 12:15:25 -0500

    Fini exercice 2

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+clear
+menu()
+{
+ echo "    Menu"
+ echo "==============="
+ echo "cvm soft"
+ echo "d=dictionnaire"
+ echo "q=quitter"
+ echo "===============" 
+}
+menu
+read -p "Entrez votre choix : " choix
+
+if test $choix = d
+then
+	./exercice1.sh
+
+elif test  $choix != d -a $choix != q
+then
+	./exercice2.sh
+else
+	echo "bye"
+fi
