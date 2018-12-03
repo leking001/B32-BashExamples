17550b38249f712d547b5b271187e36a9821f867
Miguel Dres-Barrientos <miguel4696@hotmail.com>
2018-12-03 10:41:39 -0500

    fini avec condition

--- 04-condition.sh
+++ 04-condition.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
+note=60
 
+if test $note -lt 60
+then 
+	echo"Désolé, vous devez reprendre le cours..."
+elif [[ $note -eq 60 ]]
+then 
+	echo "Vous êtes à la limite!!"
+else 
+	echo "Bravo!"
+fi
 
+read lettre 
 
+case $lettre in
+	[[:lower:]])
+		echo "minuscule"
+		;; #break
+	*) #default
+		echo "autre..."
+		;; #break
+esac
 
+l="asdf"
 
-
+if test $lettre != "a" #si on voudrait avoir ub égalité c'est juste un =
+then
+	echo "La lettre n'est  pas a"
+fi
 
